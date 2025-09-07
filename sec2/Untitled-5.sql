0861793fe83c07b56d17ad3eb7431ef575b01264
YoussefBedeer <yousser1bedeer@gmail.com>
2025-09-07 12:19:14 +0300

    basics added

--- sec2/Untitled-5.sql
+++ sec2/Untitled-5.sql
@@ -1,3 +1,65 @@
+-- create iti database 
+CREATE DATABASE ITI;
+USE ITI
+
+-- Create table students  
+
+CREATE Table students(
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    age INT check(age > 0),
+    phone_number VARCHAR(11)
+);
+DESCRIBE students;
+
+-- Insert data rows 
+
+INSERT INTO students (name, email, age, phone_number)
+VALUES
+("youssef", "yousser1bedeer@gmail.com", 22, "01226627188");
+
+-- insert multiple rows 
+INSERT INTO students (name, email, age, phone_number)
+VALUES 
+("ahmed", "ahmed@gmail.com", 21, "0123422342"),
+("ali", "ali@gmail.com", 25, "01222222131");
+
+
+-- select all data from table
+select * from students;
+
+
+-- select name and age only 
+select name, age from students;
+
+-- select student with id 3
+select * FROM students where id = 3;
+
+-- select students with age bigger than 22
+select * from students where age >= 22;
+
+-- select students with age between 21 and 23
+select * from students where age BETWEEN 21 and 23;
+
+-- like operetor
+-- select name starts with A 
+select name from students WHERE name like 'A%';
+
+-- second letter is o 
+select name from students WHERE name like '_o%'; 
+
+-- first letter A and last d
+select name from students where name like "a%d";
+
+-- update 
+
+update students set age = 30 where id = 3 or name = 'ali';
+
+-- delete row 
+delete from students WHERE id = 4;
+
+-- PRACTICE
 -- insert 3 students with unique names and ages 
 INSERT into students (name, email, age, phone_number)
 VALUES
